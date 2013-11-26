79c6d4a45ec6a8cd97d3a2e211156c46a54cb3f8
José Padilla <jpadilla@webapplicate.com>
2013-11-26 12:20:10 -0400

    WIP: Snapshots filter logic in backend

--- api/controllers/StatController.js
+++ api/controllers/StatController.js
@@ -1,5 +1,3 @@
-var moment = require("moment");
-
 /*
 StatController
 
@@ -17,17 +15,37 @@ NOTE: The code you write here supports both HTTP and Socket.io automatically.
 @docs        :: http://sailsjs.org/#!documentation/controllers
 */
 
-
 module.exports = {
+  index: function(req, res) {
+    sails.log.info('StatController index action...');
+    var filterBy = null;
+
+    if(req.query !== null) {
+      filterBy = req.query.filter;
+    } else if(req.body !== null) {
+      filterBy = req.body.filter;
+    }
+
+    Stat.find().done(function(err, stats) {
+      if(err) return res.send(err, 500);
+
+      _.forEach(stats, function(stat) {
+        stat.snapshots = stat.filterSnapshots(filterBy);
+      });
+
+      return res.json(stats);
+    });
+  },
+
   /*
       Overrides for the settings in `config/controllers.js`
       (specific to StatController)
   */
-
   _config: {
     blueprints: {
       actions: true,
-      rest: true
+      rest: true,
+      shortcuts: false
     }
   }
 };
--- api/models/Stat.js
+++ api/models/Stat.js
@@ -6,6 +6,8 @@ Stat
 @docs       :: http://sailsjs.org/#!documentation/models
 */
 
+var moment = require("moment");
+
 module.exports = {
   attributes: {
     name: {
@@ -16,6 +18,47 @@ module.exports = {
       type: "string",
       required: true
     },
-    snapshots: "array"
+    snapshots: "array",
+
+    // Filter a stats's snapshots array
+    filterSnapshots: function(filterBy) {
+      var filterFuntion;
+
+      switch(filterBy) {
+        case 'today':
+          filterFunction = function(snapshot) {
+            var today = moment().format('YYYY-MM-DD');
+            return moment(snapshot.date).format('YYYY-MM-DD') === today;
+          };
+          break;
+        case 'yesterday':
+          filterFunction = function(snapshot) {
+            var yesterday = moment().subtract('days', 1).format('YYYY-MM-DD');
+            return moment(snapshot.date).format('YYYY-MM-DD') === yesterday;
+          };
+          break;
+        case 'week':
+          filterFunction = function(snapshot) {
+            var lastWeek = moment().subtract('week', 1).format('YYYY-MM-DD'),
+              date = moment(snapshot.date).format('YYYY-MM-DD');
+            return moment(date).isAfter(lastWeek);
+          };
+          break;
+        case 'month':
+          filterFunction = function(snapshot) {
+            var lastMonth = moment().subtract('month', 1).format('YYYY-MM-DD'),
+              date = moment(snapshot.date).format('YYYY-MM-DD');
+            return moment(date).isAfter(lastMonth);
+          };
+          break;
+        default:
+          filterFunction = function() {
+            return true;
+          };
+      }
+
+      return _.filter(this.snapshots, filterFunction);
+    }
+
   }
 };
