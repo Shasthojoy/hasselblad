0fb1bd13f432efab61dab4c641cf3b96d5c595fc
José Padilla <jpadilla@webapplicate.com>
2013-11-26 17:00:11 -0400

    Setup Nodetime

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
+if(process.env.NODETIME_ACCOUNT_KEY) {
+  require('nodetime').profile({
+    accountKey: process.env.NODETIME_ACCOUNT_KEY,
+    appName: 'Hasselblad'
+  });
+}
+
 // Start sails and pass it command line arguments
 require('sails').lift(require('optimist').argv);
